9bd37ac2e5ec4f157a93967da9eea697e6c753ef
DarianGanev <darianganev83@gmail.com>
2026-07-19 17:05:27 +0300

    feat(app): compose desktop services

--- src/ApexLab.App/App.xaml.cs
+++ src/ApexLab.App/App.xaml.cs
@@ -1,12 +1,37 @@
-using System.Configuration;
-using System.Data;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ApexLab.App;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : System.Windows.Application
 {
+    private IHost? _host;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var localApplicationDataDirectory = Environment.GetFolderPath(
+            Environment.SpecialFolder.LocalApplicationData);
+        _host = AppComposition.CreateHost(localApplicationDataDirectory);
+        _host.StartAsync().GetAwaiter().GetResult();
+
+        MainWindow = _host.Services.GetRequiredService<MainWindow>();
+        MainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        try
+        {
+            _host?.StopAsync(TimeSpan.FromSeconds(5)).GetAwaiter().GetResult();
+        }
+        finally
+        {
+            _host?.Dispose();
+            _host = null;
+            base.OnExit(e);
+        }
+    }
 }
