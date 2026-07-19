5a54b63d2af8ce53c8504a7625bfd1bb3c48f5c3
DarianGanev <darianganev83@gmail.com>
2026-07-19 17:49:17 +0300

    feat(app): coordinate single-instance lifecycle

--- src/ApexLab.App/AssemblyInfo.cs
+++ src/ApexLab.App/AssemblyInfo.cs
@@ -1,4 +1,7 @@
+using System.Runtime.CompilerServices;
 using System.Windows;
+
+[assembly: InternalsVisibleTo("ApexLab.IntegrationTests")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
